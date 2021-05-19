6246d19d5a40739b38ec38033cdede5fa1acbe79
James Ohsako <johsako087@gmail.com>
2021-05-18 18:55:28 -0700

    added flash messages on success and error

--- routes/hobbies.js
+++ routes/hobbies.js
@@ -27,8 +27,10 @@ router.get('/new', (req, res) => {
   res.render('hobbies/new')
 })
 
+//Posting new hobby
 router.post('/', validateHobby, catchAsync(async (req, res, next) => {
-    const hobby = new Hobby(req.body.hobby);
+  req.flash('success', 'Successfully posted a new hobby spot!')  
+  const hobby = new Hobby(req.body.hobby);
     await hobby.save();
     res.redirect(`hobbies/${hobby._id}`)
 }))
@@ -36,11 +38,19 @@ router.post('/', validateHobby, catchAsync(async (req, res, next) => {
 //SHOW Page
 router.get('/:id', catchAsync(async (req, res) => {
   const hobby  = await Hobby.findById(req.params.id).populate('reviews');
+  if(!hobby){
+    req.flash('error', 'Cannot find the specified hobby spot.')
+    return res.redirect('/hobbies')
+  }
   res.render('hobbies/show', { hobby })
 }))
 
 router.get('/:id/edit', catchAsync(async (req, res) => {
   const hobby  = await Hobby.findById(req.params.id);
+  if(!hobby){
+    req.flash('error', 'Cannot find the specified hobby spot.')
+    return res.redirect('/hobbies')
+  }
   res.render('hobbies/edit', { hobby })
 }))
 
@@ -49,6 +59,7 @@ router.get('/:id/edit', catchAsync(async (req, res) => {
 router.put('/:id', validateHobby, catchAsync(async (req, res) => {
   const {id} = req.params;
   const hobby = await Hobby.findByIdAndUpdate(id, {...req.body.hobby});
+  req.flash('success', 'Successfully updated hobby spot')  
   res.redirect(`/hobbies/${hobby._id}`)
 }))
 
@@ -56,6 +67,7 @@ router.put('/:id', validateHobby, catchAsync(async (req, res) => {
 router.delete('/:id', catchAsync(async (req, res) => {
   const {id} = req.params;
   const hobby = await Hobby.findByIdAndDelete(id, {...req.body.hobby});
+  req.flash('success', 'Successfully removed the hobby spot')  
   res.redirect(`/hobbies`)
 }))
 
--- routes/reviews.js
+++ routes/reviews.js
@@ -28,6 +28,7 @@ router.post('/', validateReview, catchAsync(async (req, res) => {
   hobby.reviews.push(review);
   await review.save();
   await hobby.save();
+  req.flash('success', 'Your review was successfully posted!')  
   res.redirect(`/hobbies/${hobby._id}`)
 }))
 
@@ -36,6 +37,7 @@ router.delete('/:reviewId', catchAsync(async (req, res) => {
   const {id, reviewId} = req.params;
   await Hobby.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
   await Review.findByIdAndDelete(reviewId);
+  req.flash('success', 'Your review has been deleted.')  
   res.redirect(`/hobbies/${id}`)
 }))
 
